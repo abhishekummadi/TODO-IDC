3ab7517a9223d4b6dbc47bf87a35b0965787e7e8
abhishekummadi <61177346+abhishekummadi@users.noreply.github.com>
2021-09-20 16:02:36 +0530

    Add files via upload
    https://todoforidc.web.app/ - hosted from firebase

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBqmCl7ZFAoXH0Jt6ZF-eD81Cc1EKVO3y4",
+    authDomain: "todoforidc.firebaseapp.com",
+    projectId: "todoforidc",
+    storageBucket: "todoforidc.appspot.com",
+    messagingSenderId: "347150943943",
+    appId: "1:347150943943:web:6f2e3cb4b8378fe7e8e0e2",
+    measurementId: "G-7ZDG4VRSK2"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
